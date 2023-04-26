d5b6413090670a6eb65a8767bcf52f85ed970c59
gambarov <gambarov.20015960@gmail.com>
2023-04-27 02:34:53 +0500

    config validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule, { cors: true });
+    app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(PORT, () => {
         console.log(`Server started on port = ${PORT}`);
