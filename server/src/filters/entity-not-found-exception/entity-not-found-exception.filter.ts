34d68dda73219346d660da2a6389333a5649b230
gambarov <gambarov.20015960@gmail.com>
2023-04-27 14:43:48 +0500

    add 404 filter handler

---
+++ server/src/filters/entity-not-found-exception/entity-not-found-exception.filter.ts
@@ -0,0 +1,18 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Response } from 'express';
+import { EntityNotFoundError } from 'typeorm';
+
+@Catch(EntityNotFoundError)
+export class EntityNotFoundExceptionFilter<T> implements ExceptionFilter {
+    catch(exception: EntityNotFoundError, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        return response.status(404).json({
+            message: {
+                statusCode: 404,
+                error: 'Не найден',
+                message: exception.message,
+            },
+        });
+    }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { useContainer } from 'class-validator/types/container';
+import { useContainer } from 'class-validator';
+import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception/entity-not-found-exception.filter';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule, { cors: true });
     app.useGlobalPipes(new ValidationPipe());
-
+    app.useGlobalFilters(new EntityNotFoundExceptionFilter());
     useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
     await app.listen(PORT, () => {
