81c165a22a35bd43c0294ad80db530d3ae703f7a
gambarov <gambarov.20015960@gmail.com>
2023-04-27 20:52:31 +0500

    config swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception/entity-not-found-exception.filter';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 3000;
@@ -10,6 +12,10 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalFilters(new EntityNotFoundExceptionFilter());
     useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+    const config = new DocumentBuilder().build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(PORT, () => {
         console.log(`Server started on port = ${PORT}`);
