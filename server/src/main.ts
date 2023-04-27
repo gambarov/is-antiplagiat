cc673937d6a62495cb80e1602fe3a26acc1d5cc5
gambarov <gambarov.20015960@gmail.com>
2023-04-27 13:49:57 +0500

    enable DI for class-validator

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator/types/container';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule, { cors: true });
     app.useGlobalPipes(new ValidationPipe());
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
     await app.listen(PORT, () => {
         console.log(`Server started on port = ${PORT}`);
