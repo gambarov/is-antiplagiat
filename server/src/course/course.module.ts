a0465927e12100070f5cc749712e45c592f89b6a
gambarov <gambarov.20015960@gmail.com>
2023-04-28 22:33:18 +0500

    imports ass entity into course module

--- server/src/course/course.module.ts
+++ server/src/course/course.module.ts
@@ -3,11 +3,12 @@ import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from './course.entity';
+import { AssignmentEntity } from 'src/assignment/assignment.entity';
 
 @Module({
     controllers: [CourseController],
     providers: [CourseService],
     exports: [CourseService],
-    imports: [TypeOrmModule.forFeature([CourseEntity])],
+    imports: [TypeOrmModule.forFeature([CourseEntity, AssignmentEntity])],
 })
 export class CourseModule {}
