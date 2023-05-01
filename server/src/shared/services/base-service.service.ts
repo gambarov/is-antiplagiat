1ee6db107e7aa29a765d0db3d5dfa44e47525f8b
gambarov <gambarov.20015960@gmail.com>
2023-05-02 04:10:59 +0500

    add base entity & service classes

---
+++ server/src/shared/services/base-service.service.ts
@@ -0,0 +1,47 @@
+import {
+    FindManyOptions,
+    Repository,
+    FindOptionsWhere,
+    FindOneOptions,
+} from 'typeorm';
+import { PaginatedOptionsDTO } from 'src/shared/dto/paginated-meta-params.dto';
+import { PaginatedDTO } from 'src/shared/dto/paginated.dto';
+import { PaginatedMetaDTO } from 'src/shared/dto/paginated-meta.dto';
+import { BaseEntity } from '../entities/base-entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BaseService<T extends BaseEntity> {
+    constructor(private repo: Repository<T>) {}
+
+    async findByIdOrFail(id: number, options?: FindOneOptions<T>) {
+        const findOptions: FindOneOptions<T> = {
+            ...options,
+            where: {
+                id,
+            } as unknown as FindOptionsWhere<T>,
+        };
+        return await this.repo.findOneOrFail(findOptions);
+    }
+
+    async findAllWithPagination(
+        optionsDTO: PaginatedOptionsDTO,
+        options?: FindManyOptions<T>,
+    ): Promise<PaginatedDTO<T>> {
+        const [entities, itemCount] = await this.repo.findAndCount({
+            ...options,
+            take: optionsDTO.take,
+            skip: optionsDTO.skip,
+        });
+
+        const meta = new PaginatedMetaDTO({
+            itemCount,
+            paginatedOptionsDTO: optionsDTO,
+        });
+
+        return {
+            data: entities,
+            meta,
+        };
+    }
+}
