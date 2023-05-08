b80e1af217269b3d7d6b574a27f2fd8effd5f7dc
gambarov <gambarov.20015960@gmail.com>
2023-05-08 17:43:49 +0500

    feat(token-service): TokenPair interface

--- server/src/auth/token.service.ts
+++ server/src/auth/token.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { UserEntity } from 'src/user/entities/user.entity';
+import { TokenPair } from './interfaces/token-pair.interface';
 
 @Injectable()
 export class TokenService {
@@ -10,7 +11,7 @@ export class TokenService {
         private readonly configService: ConfigService,
     ) {}
 
-    async generateTokens(payload: Partial<UserEntity>) {
+    async generateTokens(payload: Partial<UserEntity>): Promise<TokenPair> {
         const accessToken = await this.jwtService.signAsync(payload, {
             expiresIn: this.configService.get('JWT_ACCESS_EXPIRATION'),
             secret: this.configService.get('JWT_ACCESS_SECRET'),
