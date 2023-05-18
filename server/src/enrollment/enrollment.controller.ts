613cb472ec19fb3ea6ed95b2bf5173cd102b5bdb
gambarov <gambarov.20015960@gmail.com>
2023-05-18 21:42:09 +0500

    feat(enrollment): in progress

--- server/src/enrollment/enrollment.controller.ts
+++ server/src/enrollment/enrollment.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Query,
+} from '@nestjs/common';
+import { EnrollmentService } from './enrollment.service';
+import { EnrollmentEntity } from './entities/enrollment.entity';
+import { ResponseManyDTO } from '../shared/crud/dto/response-many.dto';
+import { RequestManyDTO } from '../shared/crud/dto/request-many.dto';
 
 @Controller('enrollment')
-export class EnrollmentController {}
+export class EnrollmentController {
+    constructor(private enrollmentService: EnrollmentService) {}
+
+    @Get()
+    async findMany(
+        @Query() optionsDTO: RequestManyDTO,
+    ): Promise<ResponseManyDTO<EnrollmentEntity>> {
+        return await this.enrollmentService.findMany(optionsDTO);
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number) {
+        return await this.enrollmentService.findByIdOrFail(id);
+    }
+
+    @Post()
+    async createOne(@Body() dto: EnrollmentEntity) {
+        return await this.enrollmentService.createOne(dto);
+    }
+
+    @Post(':id')
+    async updateOne(@Param('id') id: number, @Body() dto: EnrollmentEntity) {
+        return await this.enrollmentService.updateOne(id, dto);
+    }
+
+    @Delete(':id')
+    async deleteOne(@Param('id') id: number) {
+        return await this.enrollmentService.deleteOne(id);
+    }
+}
