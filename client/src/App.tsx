c115a0753fa60b7d38151807f7e6e658006d9c14
gambarov <gambarov.20015960@gmail.com>
2023-05-20 22:24:58 +0500

    upd last client demo before resetting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,11 @@ function App() {
     return (
         <>
             <Navbar />
-            <Layout.Content style={{ padding: '25px 200px 300px 350px' }}>
+            {/* top | right | bottom | left */}
+            <Layout.Content style={{ padding: '0 15% 0 15%', minHeight: '100vh' }}>
                 <MainPage />
             </Layout.Content>
+            <Layout.Footer style={{ textAlign: 'center', position: "sticky", bottom: "0" }}>IS-Antiplagiat ©2023 Created by DG</Layout.Footer>
         </>
     );
 }
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,103 +1,167 @@
-import React, { FC, useState, useEffect } from 'react';
-import { Row, Col, Button, Upload, Space, Table, Tag } from 'antd';
-import type { UploadProps, UploadFile } from 'antd';
-import type { RcFile } from 'antd/es/upload/interface';
-import type { ColumnsType } from 'antd/es/table';
+import React, { FC, useState, useEffect } from "react";
+import { Row, Col, Button, Upload, Table, Breadcrumb, theme, notification, Card } from "antd";
+import type { UploadProps, UploadFile } from "antd";
+import type { RcFile } from "antd/es/upload/interface";
+import type { ColumnsType } from "antd/es/table";
+import axios from "axios";
 
-type WorkResult = {
+type Submission = {
+    id?: number;
+    answers?: Answer[];
+    status?: string;
+    comment?: string;
+};
+
+type AnswerResult = {
     id?: number;
     DocId?: number;
     IsSuspicious?: boolean;
     Legal?: number;
     Plagiarism?: number;
     SelfCite?: number;
-    Unknown?: number,
+    Unknown?: number;
     Status?: string;
-}
+};
 
-type Work = {
+type Answer = {
     id?: number;
-    name?: string;
     status?: string;
-    result?: WorkResult;
-}
+    result?: AnswerResult;
+};
 
 type TableData = {
-    key?: number | string,
-    name?: string,
-    status?: string,
-    Plagiarism?: number,
-    Legal?: number,
-    SelfCite?: number,
-    Unknown?: number
-}
+    key?: number | string;
+    name?: any;
+    status?: string;
+    Plagiarism?: number;
+    Legal?: number;
+    SelfCite?: number;
+    Unknown?: number;
+};
 
 const columns: ColumnsType<TableData> = [
     {
-        key: 'name',
-        title: 'Работа',
-        dataIndex: 'name',
+        key: "id",
+        title: "Работа",
+        dataIndex: "name",
     },
     {
-        key: 'status',
-        title: 'Статус',
-        dataIndex: 'status',
+        key: "status",
+        title: "Статус",
+        dataIndex: "status",
     },
     {
-        key: 'Plagiarism',
-        title: 'Процент заимствования',
-        dataIndex: 'Plagiarism',
+        key: "Plagiarism",
+        title: "% заимствования",
+        dataIndex: "Plagiarism",
     },
     {
-        key: 'Legal',
-        title: 'Процент цитирования',
-        dataIndex: 'Legal',
+        key: "Legal",
+        title: "% цитирования",
+        dataIndex: "Legal",
     },
     {
-        key: 'SelfCite',
-        title: 'Процент самоцитирования',
-        dataIndex: 'SelfCite',
+        key: "SelfCite",
+        title: "% самоцитирования",
+        dataIndex: "SelfCite",
     },
     {
-        key: 'Unknown',
-        title: 'Процент оригинальности',
-        dataIndex: 'Unknown',
+        key: "Unknown",
+        title: "% оригинальности",
+        dataIndex: "Unknown",
     },
-]
+];
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
+
+const defaultSubmission: Submission = {
+    answers: [],
+    status: 'NOT_SUBMITTED',
+    comment: '',
+}
 
 export const MainPage: FC = () => {
-
     const [fileList, setFileList] = useState<UploadFile[]>([]);
     const [uploading, setUploading] = useState(false);
+
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    const [api, contextHolder] = notification.useNotification();
+
+    const openNotificationWithIcon = (type: NotificationType) => {
+        api[type]({
+            message: 'Уведомление',
+            description:
+                'Работа успешно загружена и отправлена на проверку!',
+        });
+    };
+
+    const handleCheckStatus = () => {
+        const answer = answers[answers.length - 1];
+        axios<Answer>({
+            method: "post",
+            url: "http://localhost:4000/answers/status",
+            data: { 'docId': answer.result?.DocId?.toString() as string },
+        })
+            .then(function (response) {
+                console.log(response)
+
+                axios<Submission>({
+                    method: "get",
+                    url: "http://localhost:4000/submissions/3",
+                })
+                    .then(function (response) {
+                        console.log(response.data);
+                        if (response.data != null) {
+                            setSubmission(response.data);
+                            setAnswers(response.data.answers as Answer[]);
+                        }
+                    })
+                    .catch(function (response) {
+                        console.log(response);
+                    });
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+    }
 
     const handleUpload = () => {
         const formData = new FormData();
         fileList.forEach((file) => {
-            formData.append('file', file as RcFile);
+            formData.append("file", file as RcFile);
         });
-        formData.append('name', 'testwork');
+        formData.append('submission_id', '3');
         setUploading(true);
-        // You can use any AJAX library you like
-        fetch('http://localhost:4000/works/upload', {
-            method: 'POST',
-            body: formData,
+
+        axios<Answer>({
+            method: "post",
+            url: "http://localhost:4000/answers/upload",
+            data: formData,
+            headers: { "Content-Type": "multipart/form-data" },
         })
-            .then((res) => res.json())
-            .then((res) => {
-                setFileList([]);
-                console.log('upload successfully.');
-                console.log(res);
-                const formData = new FormData();
-                formData.append('docId', res.result.DocId);
-                fetch('http://localhost:4000/works/check', {
-                    method: 'POST',
-                    body: formData,
+            .then(function (response) {
+                setSubmission({ ...submission, 'status': 'PENDING' })
+                setAnswers([...answers, response.data])
+
+                axios({
+                    method: "post",
+                    url: "http://localhost:4000/answers/check",
+                    data: { 'docId': response.data.result?.DocId }
                 })
-                    .then((res) => res.json())
-                    .then((res) => { console.log(res) })
+                    .then((res) => {
+                        openNotificationWithIcon('success');
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    })
             })
-            .catch(() => {
-                console.log('upload failed.');
+            .catch(function (response) {
+                //handle error
+                console.log(response);
             })
             .finally(() => {
                 setUploading(false);
@@ -119,50 +183,98 @@ export const MainPage: FC = () => {
         fileList,
     };
 
-    const [works, setWorks] = useState<Work[]>([]);
+    const [submission, setSubmission] = useState<Submission>(defaultSubmission);
+    const [answers, setAnswers] = useState<Answer[]>([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/works', {
-            method: 'GET',
+        axios<Submission>({
+            method: "get",
+            url: "http://localhost:4000/submissions/3",
         })
-            .then((res) => res.json())
-            .then((res) => {
-                setWorks(res);
+            .then(function (response) {
+                console.log(response.data);
+                if (response.data != null) {
+                    setSubmission(response.data);
+                    setAnswers(response.data.answers as Answer[]);
+                }
             })
-            .catch(() => {
-                console.log('upload failed.');
+            .catch(function (response) {
+                console.log(response);
             });
-    }, [])
+    }, []);
 
-    const dataSource = works.map<TableData>((work) => ({
-        key: work.id,
-        name: work.name,
-        status: work.status,
-        Plagiarism: work.result?.Plagiarism,
-        Legal: work.result?.Legal,
-        SelfCite: work.result?.SelfCite,
-        Unknown: work.result?.Unknown
-    }))
+    const dataSource = answers.map<TableData>((answer) => ({
+        key: answer.id,
+        name: answer.id,
+        status: answer.status === 'PENDING' ? "На проверке" : "Проверена",
+        Plagiarism: answer.result?.Plagiarism,
+        Legal: answer.result?.Legal,
+        SelfCite: answer.result?.SelfCite,
+        Unknown: answer.result?.Unknown,
+    }));
+
+    const translateSubmissionStatus = () => {
+        switch (submission.status) {
+            case 'NOT_SUBMITTED':
+                return 'Работа не отправлялась'
+            case 'PENDING':
+                return 'Отправлена на проверку'
+            case 'ON_REVIEW':
+                return 'На проверке'
+            case 'PASSED':
+                return 'Прошла проверку'
+            case 'FAILED':
+                return 'Непрошла проверку'
+            default:
+                return submission.status;
+        }
+    }
 
     return (
-        <Row>
-            <Col>
-                <Upload {...props}>
-                    <Button>Выберите файл</Button>
-                </Upload>
-                <Button
-                    type="primary"
-                    onClick={handleUpload}
-                    disabled={fileList.length === 0}
-                    loading={uploading}
-                    style={{ marginTop: 16 }}
-                >
-                    {uploading ? 'Идет загрузка...' : 'Начать'}
-                </Button>
-            </Col>
-            <Col>
-                <Table columns={columns} dataSource={dataSource} />
-            </Col>
-        </Row>
+        <>
+            {contextHolder}
+            <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Курс №1</Breadcrumb.Item>
+                <Breadcrumb.Item>Задание №1</Breadcrumb.Item>
+                <Breadcrumb.Item>Загрузка по заданию №1</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: colorBgContainer, padding: '25px', minHeight: '100vh' }}>
+                <Row justify="space-around" align="top">
+                    <Col span={4}>
+                        <Card title="Загрузка ответа">
+                            <Upload {...props}>
+                                <Button>Выберите файл</Button>
+                            </Upload>
+                            <Button
+                                type="primary"
+                                onClick={handleUpload}
+                                disabled={fileList.length === 0}
+                                loading={uploading}
+                                style={{ marginTop: 16 }}
+                            >
+                                {uploading ? "Идет загрузка..." : "Начать"}
+                            </Button>
+                        </Card>
+                    </Col>
+                    <Col span={19}>
+                        <Card title='Состояние прогресса'>
+                            Состояние ответа: {translateSubmissionStatus()}
+                            <br/>
+                            <br/>
+                            {submission.comment ? 'Комментарий: ' + submission.comment : ''}
+                            <Table columns={columns} dataSource={dataSource} />
+                            <Button
+                                type="primary"
+                                onClick={handleCheckStatus}
+                                disabled={answers.length === 0}
+                                style={{ marginTop: 16 }}
+                            >
+                                Обновить результаты
+                            </Button>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        </>
     );
 };
